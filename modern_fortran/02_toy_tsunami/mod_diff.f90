efd375934d08fff5bf7c675fba136e9d60617084
Bizovi <bizovim@gmail.com>
2020-12-01 02:38:38 +0200

    Add coupled equations

--- modern_fortran/02_toy_tsunami/mod_diff.f90
+++ modern_fortran/02_toy_tsunami/mod_diff.f90
@@ -2,19 +2,36 @@ module mod_diff
 
     use iso_fortran_env, only: int32, real32
     implicit none
-    public :: diff
+    public  :: diff_centered
+    private :: diff_upwind
 
 contains
     ! calculates the difference in space of a real-valued input vector
     ! hides the details of the implementation, e.g. the boundary condition
-    pure function diff(x) result(dx)
+    pure function diff_upwind(x) result(dx)
         real(real32), intent(in) :: x(:)  ! : assumes the shape of the passed array
         real(real32)   :: dx(size(x))     ! same size as x
         integer(int32) :: im
 
         im = size(x)
         dx(1) = x(1) - x(im)            ! handle the boundary condition
-        dx(2:im) = x(2:im) - x(1:im-1)  ! finite difference of other elements of x(:)
-    end function diff
+
+        ! TODO(Mihai) <| need a centered difference
+        ! water doesn't only flow from left to right
+        dx(2:im) = x(2:im) - x(1:im-1)
+    end function diff_upwind
+
+    ! Assumes a window of two, calculating the average, moves in both directions
+    pure function diff_centered(x) result(dx)
+        real(real32), intent(in) :: x(:)
+        real(real32)   :: dx(size(x))
+        integer(int32) :: im
+        im = size(x)
+
+        dx(1)  = x(2) - x(im)   ! boundary on the left
+        dx(im) = x(1) - x(im-1) ! boundary on the right
+        dx(2:im-1) = x(3:im) - x(1:im-2)
+        dx = 0.5 * dx
+    end function diff_centered
 
 end module mod_diff
--- modern_fortran/02_toy_tsunami/tsunami.f90
+++ modern_fortran/02_toy_tsunami/tsunami.f90
@@ -2,32 +2,36 @@ program tsunami
 
     use iso_fortran_env, only: int32, real32
     use mod_initial, only: set_gaussian
-    use mod_diff, only: diff
+    use mod_diff, only: diff => diff_centered
     implicit none
 
     ! ========= Declare vars and constants =======
     ! ============================================
-    integer(int32), parameter :: grid_size = 100          ! constant, same line required init.
-    integer(int32), parameter :: num_time_steps = 100     ! simulation length
-    real(real32),   parameter :: dt = 1., dx = 1., c = 1. ! time step, grid step, bg flow speed
-    integer(int32), parameter :: icenter = 25             ! mean of gaussian / central index
-    real(real32),   parameter :: decay = 0.02             ! shape factor (1/sigma)
+    integer(int32), parameter :: grid_size = 100
+    integer(int32), parameter :: num_time_steps = 5000
+    real(real32),   parameter :: dt = 0.02, dx = 1., g = 9.81
+    real(real32),   parameter :: hmean = 10.
 
-    integer(int32) :: n           ! n - time step idx, i - becomes vectorized
-    real(real32) :: h(grid_size)  ! real, dimension(grid_size), dh calculated on-the-fly
+    integer(int32), parameter :: icenter = 25
+    real(real32),   parameter :: decay = 0.02
+
+    integer(int32) :: n
+    real(real32)   :: h(grid_size), u(grid_size)
 
     if (grid_size <= 0) stop 'grid_size must be > 0'
     if (dt <= 0)        stop 'time step dt must be > 0'
     if (dx <= 0)        stop 'grid spacing dx must be > 0'
-    if (c <= 0)         stop 'background flow speed c must be > 0'
 
     ! ========= The computation logic ============
     ! ============================================
     call set_gaussian(h, icenter, decay)
+    u = 0  ! initialize the velocity array
+    
     print *, 0, h
-
     time_loop: do n = 1, num_time_steps
-        h = h - c*diff(h) / dx*dt
+        ! the equations are coupled
+        u = u - (u * diff(u) + g * diff(h)) / dx * dt
+        h = h - diff(u * (hmean + h)) / dx*dt
         print *, n, h
     end do time_loop
     
